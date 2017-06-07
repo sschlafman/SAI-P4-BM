9b8d65102726ea2c86a418fad6b13dd01a34d9c5
Omer Shabtai <snifbh@gmail.com>
2017-06-07 14:25:49 +0300

    add make pkg script

--- p4-softswitch/travis/install-thrift.sh
+++ p4-softswitch/travis/install-thrift.sh
@@ -2,7 +2,6 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $THIS_DIR/common.sh
-sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev 
 check_lib libthrift libthrift-0.9.2
 set -e
 wget http://archive.apache.org/dist/thrift/0.9.2/thrift-0.9.2.tar.gz
